d5e052c79200c0852c80e3a947a017577fc26542
Diego 'Flameeyes' Pettenò <flameeyes@gmail.com>
2007-12-18 16:09:51 +0100

    Add a test for the UnsupportedElfVersion exception.

--- tests/tc_exceptions.rb
+++ tests/tc_exceptions.rb
@@ -65,4 +65,19 @@ class TC_Exceptions < Test::Unit::TestCase
 
     assert(exception_received, "Elf::File::InvalidDataEncoding exception not received.")
   end
+
+  def test_unsupported_version
+    assert(File.exist?(TestDir + "invalid_unsupportedversion"),
+           "Missing test file invalid_unsupportedversion")
+
+    exception_received = false
+    begin
+      elf = Elf::File.new(TestDir + "invalid_unsupportedversion")
+      elf.close
+    rescue Elf::File::UnsupportedElfVersion
+      exception_received = true
+    end
+
+    assert(exception_received, "Elf::File::UnsupportedElfVersion exception not received.")
+  end
 end
