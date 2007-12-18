5c499cc5ef809e4780486df724b5f340c3ec569b
Diego 'Flameeyes' Pettenò <flameeyes@gmail.com>
2007-12-18 15:05:32 +0100

    Add a new test unit for exceptions, begin with a file that is not an Elf file.

---
+++ tests/tc_exceptions.rb
@@ -0,0 +1,41 @@
+# Copyright 2007, Diego "Flameeyes" Pettenò <flameeyes@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this generator; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+require 'test/unit'
+require 'pathname'
+require 'elf'
+
+class TC_Exceptions < Test::Unit::TestCase
+  TestDir = Pathname.new(__FILE__).dirname + "binaries"
+  def setup
+    [ "invalid_nonelf" ].each do |file|
+      assert(File.exist?(TestDir + file),
+             "Missing test file #{file}")
+    end
+  end
+
+  def test_notanelf
+    exception_received = false
+    begin
+      elf = Elf::File.new(TestDir + "invalid_nonelf")
+      elf.close
+    rescue Elf::File::NotAnELF
+      exception_received = true
+    end
+
+    assert(exception_received, "Elf::File::NotAnElf exception not received")
+  end
+end
