7fddabd08aec8563be17c5a71dc469df0a743f0b
Diego 'Flameeyes' Pettenò <flameeyes@gmail.com>
2008-04-30 14:35:40 +0200

    Start working on an idea given to me by exg, a tool to check for non-static symbols not used but in their own translation unit.

---
+++ tools/missingstatic.rb
@@ -0,0 +1,113 @@
+#!/usr/bin/env ruby
+# Copyright © 2008, Diego "Flameeyes" Pettenò <flameeyes@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this generator; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+# check for functions that are not used but in their translation unit
+
+require 'elf'
+require 'getoptlong'
+require 'set'
+
+opts = GetoptLong.new(
+  # Read the files to scan from a file rather than commandline
+  ["--filelist", "-f", GetoptLong::REQUIRED_ARGUMENT],
+  # Exclude functions with a given prefix (exported functions)
+  ["--exclude-prefix", "-x", GetoptLong::REQUIRED_ARGUMENT]
+)
+
+exclude_prefixes = []
+files_list = nil
+
+opts.each do |opt, arg|
+  case opt
+  when '--filelist'
+    if arg == '-'
+      files_list = $stdin
+    else
+      files_list = File.new(arg)
+    end
+  when '--exclude-prefix'
+    exclude_prefixes << arg
+  end
+end
+
+$all_defined = Set.new
+$all_using = Set.new
+$symbol_objects = {}
+
+def scanfile(filename)
+  begin
+    Elf::File.open(filename) do |elf|
+      if elf.type != Elf::File::Type::Rel
+        $stderr.puts "missingstatic.rb: #{file}: not an object file"
+        next
+      end
+      if elf.sections['.symtab'] == nil
+        $stderr.puts "missingstatic.rb: #{file}: no .symtab section found"
+        next
+      end
+
+      # Gather all the symbols, defined and missing in the translation unit
+      this_defined = Set.new
+      this_using = Set.new
+
+      elf.sections['.symtab'].symbols.each do |sym|
+        if sym.section == Elf::Section::Undef
+          this_using << sym.name
+        elsif sym.bind == Elf::Symbol::Binding::Local
+          next
+        elsif sym.section.is_a? Elf::Section
+          this_defined << sym.name
+          $symbol_objects[sym.name] = filename
+        end
+      end
+
+      # Remove the symbols that are being used which are defined in this
+      # translation unit. We need to find which symbols are _only_ used
+      # in the same translation unit anyway.
+      this_using.subtract this_defined
+
+      $all_using.merge this_using
+      $all_defined.merge this_defined
+    end
+  rescue Errno::ENOENT
+    $stderr.puts "missingstatic.rb: #{file}: no such file"
+  rescue Elf::File::NotAnELF
+    $stderr.puts "missingstatic.rb: #{file}: not a valid ELF file."
+  end
+end
+
+# If there are no arguments passed through the command line
+# consider it like we're going to act on stdin.
+if not files_list and ARGV.size == 0
+  files_list = $stdin
+end
+
+if files_list
+  files_list.each_line do |file|
+    scanfile(file.chomp)
+  end
+else
+  ARGV.each do |file|
+    scanfile(file)
+  end
+end
+
+$all_defined.subtract $all_using
+
+$all_defined.each do |sym|
+  puts "#{sym} (#{$symbol_objects[sym]})"
+end
