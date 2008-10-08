f8da4320da175574365199eddf218f99c18066ef
Diego 'Flameeyes' Pettenò <flameeyes@gmail.com>
2008-10-08 14:24:10 +0200

    Add a test for the nm_code funtion in Elf::Symbol.
    Right now only Linux/AMD64 is tested for this, but this might change
    in the future.

---
+++ tests/binaries/symboltypes.c
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2008 by Diego "Flameeyes" Pettenò <flameeyes@gmail.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND INTERNET SOFTWARE CONSORTIUM DISCLAIMS
+ * ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL INTERNET SOFTWARE
+ * CONSORTIUM BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+ * SOFTWARE.
+ */
+
+/* Attributes cold and hot needs to be supported */
+#if !defined(__GNUC__) || defined(__ICC) || \
+  !(__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 3))
+# error "This testcase has to be built with GCC 4.3"
+#endif
+
+/* We need .data.rel symbols to be emitted */
+#if !defined(__PIC__)
+# error "This testcase has to be built with PIC enabled"
+#endif
+
+/* We don't want to fill the static variables with used attributes */
+#if defined(__OPTIMIZE__)
+# error "This testcase cannot be built with optimisations"
+#endif
+
+char external_variable[] = "foo";
+static char static_variable[] = "foo";
+
+const char external_constant[] = "foo";
+static const char static_constant[] = "foo";
+
+const char *relocated_external_variable = "foo";
+const char *const relocated_external_constant = "foo";
+
+static const char *relocated_static_variable = "foo";
+static const char *const relocated_static_constant = "foo";
+
+char external_uninitialised_variable;
+static char static_uninitialised_variable;
+
+__thread char external_tls_variable[] = "foo";
+static __thread char static_tls_variable[] = "foo";
+
+__thread char external_uninitialised_tls_variable;
+static __thread char static_uninitialised_tls_variable;
+
+__thread const char *relocated_external_tls_variable = "foo";
+static __thread const char *relocated_static_tls_variable = "foo";
+
+void external_function() {
+}
+
+static void static_function() {
+}
+
+void __attribute__((cold)) external_cold_function() {
+}
+
+static __attribute__((cold)) void static_cold_function() {
+}
+
+void __attribute__((hot)) external_hot_function() {
+}
+
+static void  __attribute__((hot)) static_hot_function() {
+}
---
+++ tests/tc_nmcodes.rb
@@ -0,0 +1,111 @@
+# Copyright 2008, Diego "Flameeyes" Pettenò <flameeyes@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this generator; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+require 'test/unit'
+require 'pathname'
+require 'elf'
+
+# Tests for nm(1)-style codes support in Ruby-Elf
+#
+# This test will load the file built from the symboltypes.c source
+# file and will ensure that the symbols in there reports the correct
+# nm(1)-style code.
+#
+# Right now only Linux/AMD64 version of the file is tested
+class TC_SunWSpecific < Test::Unit::TestCase
+  TestDir = Pathname.new(__FILE__).dirname + "binaries"
+
+  def setup
+    @elf = Elf::File.new(TestDir + "linux_amd64_symboltypes.o")
+
+    assert(@elf.has_section?(".symtab"),
+           "ELF file #{TestDir + "linux_amd64_symboltypes.o"} lacks symbol table!")
+  end
+
+  def teardown
+    @elf.close
+  end
+
+  def dotest_symbols(table)
+    table.each_pair do |sym, code|
+      assert_equal code, @elf[".symtab"][sym].nm_code, "Testing #{sym}"
+    end
+  end
+
+  # Test the general symbols
+  def test_general
+    dotest_symbols({ "symboltypes.c" => 'a'})
+  end
+
+  # Test variables
+  def test_variables
+    dotest_symbols({ "external_variable"               => 'D',
+                     "static_variable"                 => 'd',
+                     "relocated_external_variable"     => 'D',
+                     "relocated_static_variable"       => 'd',
+                     "external_uninitialised_variable" => 'C',
+                     "static_uninitialised_variable"   => 'b' })
+  end
+
+  # Test TLS variables
+  def test_tls
+    dotest_symbols({ "external_tls_variable"               => 'D',
+                     "static_tls_variable"                 => 'd',
+                     "external_uninitialised_tls_variable" => 'B',
+                     "static_uninitialised_tls_variable"   => 'b',
+                     "relocated_external_tls_variable"     => 'D',
+                     "relocated_static_tls_variable"       => 'd' })
+  end
+
+  # Test constants
+  def test_constants
+    dotest_symbols({ "external_constant"           => 'R',
+                     "static_constant"             => 'r',
+                     "relocated_external_constant" => 'D',
+                     "relocated_static_constant"   => 'd' })
+  end
+
+  # Test functions
+  def test_functions
+    dotest_symbols({ "external_function"      => 'T',
+                     "static_function"        => 't',
+                     "external_cold_function" => 'T',
+                     "static_cold_function"   => 't',
+                     "external_hot_function"  => 'T',
+                     "static_hot_function"    => 't' })
+  end
+
+  # Test section names symbols
+  #
+  # Each section in an ELF file has its equivalent symbol name, test
+  # the presence of (some) section names in symbols and their value
+  #
+  # TODO: these symbols don't appear to be loaded by ruby-elf!
+  # def test_sections
+  #   dotest_symbols({ ".bss"               => 'b',
+  #                    ".comment"           => 'n',
+  #                    ".data"              => 'd',
+  #                    ".data.rel.local"    => 'd',
+  #                    ".data.rel.ro.local" => 'd',
+  #                    ".eh_frame"          => 'r',
+  #                    ".note.GNU-stack"    => 'n',
+  #                    ".rodata"            => 'r',
+  #                    ".tss"               => 'b',
+  #                    ".tdata"             => 'd',
+  #                    ".text"              => 't' })
+  # end
+
+end
