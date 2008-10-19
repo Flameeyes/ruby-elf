479b0593c76c338eea61a8491d0dcba11fba560d
Diego 'Flameeyes' Pettenò <flameeyes@gmail.com>
2008-10-19 21:00:44 +0200

    Start adding further tests on the .dynamic section handling.
    In particular, test for presence and value of the NEEDED entry for
    libc.so. Note, this required a rebuild of the dynamic executable with
    icc since otherwise it links in libm, libdl and libgcc_s even though
    they are not needed at all.

--- tests/tc_dynamic_executable.rb
+++ tests/tc_dynamic_executable.rb
@@ -44,43 +44,69 @@ class TC_Dynamic_Executable < Elf::TestExecutable
     end
   end
 
+  # Test some values in the .dynamic section:
+
+  # Test the NEEDED line in the dynamic section
+  #
+  # The subclasses will define the proper value to use since it's
+  # operating system dependent.
+  def test_dynamic_needed
+    @elf[".dynamic"].entries.each do |entry|
+      if entry[:type] == Elf::Dynamic::Type::Needed
+        assert_equal(self.class::ExpectedLibC, entry[:parsed],
+                     "C library NEEDED differs from expectation")
+        return
+      end
+    end
+
+    flunk("NEEDED entry not found in .dynamic")
+  end
+
   class LinuxX86 < self
     Filename = "linux_x86_" + BaseFilename
+    ExpectedLibC = "libc.so.6"
     include Elf::TestExecutable::LinuxX86
   end
 
   class LinuxAMD64 < self
     Filename = "linux_amd64_" + BaseFilename
+    ExpectedLibC = "libc.so.6"
     include Elf::TestExecutable::LinuxAMD64
   end
 
   class LinuxAMD64_ICC < self
     Filename = "linux_amd64_icc_" + BaseFilename
+    ExpectedLibC = "libc.so.6"
     include Elf::TestExecutable::LinuxAMD64
   end
 
   class LinuxAMD64_SunStudio < self
     Filename = "linux_amd64_suncc_" + BaseFilename
+    ExpectedLibC = "libc.so.6"
     include Elf::TestExecutable::LinuxAMD64
   end
 
   class LinuxSparc < self
     Filename = "linux_sparc_" + BaseFilename
+    ExpectedLibC = "libc.so.6"
     include Elf::TestExecutable::LinuxSparc
   end
 
   class LinuxArm < self
     Filename = "linux_arm_" + BaseFilename
+    ExpectedLibC = "libc.so.6"
     include Elf::TestExecutable::LinuxArm
   end
 
   class SolarisX86_GCC < self
     Filename = "solaris_x86_gcc_executable"
+    ExpectedLibC = "libc.so.1"
     include Elf::TestExecutable::SolarisX86_GCC
   end
 
   class SolarisX86_SunStudio < self
     Filename = "solaris_x86_suncc_executable"
+    ExpectedLibC = "libc.so.1"
     include Elf::TestExecutable::SolarisX86_SunStudio
   end
 
