0e04caa23f7ecafda1aa9d642dcd0ebe53660a35
Diego E. 'Flameeyes' Pettenò <flameeyes@gmail.com>
2008-12-28 15:20:17 +0100

    Remove stray and pointless semicolon.

--- tools/link-collisions/analyse.rb
+++ tools/link-collisions/analyse.rb
@@ -49,7 +49,7 @@ db = PGconn.open(pg_params)
 db.exec("PREPARE getinstances (text, text) AS
          SELECT path FROM symbols INNER JOIN objects ON symbols.object = objects.id WHERE symbol = $1 AND abi = $2")
 
-db.exec("SELECT * FROM duplicate_symbols;").each do |row|
+db.exec("SELECT * FROM duplicate_symbols").each do |row|
   outfile.puts "Symbol #{row[0]} (#{row[1]}) present #{row[2]} times"
   db.exec( "EXECUTE getinstances ('#{row[0]}', '#{row[1]}')" ).each do |path|
     outfile.puts "  #{path[0]}"
