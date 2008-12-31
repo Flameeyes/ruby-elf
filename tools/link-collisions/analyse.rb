b8c5450589cd080cb8a586676e1b75bd000bc586
Diego E. 'Flameeyes' Pettenò <flameeyes@gmail.com>
2008-12-31 20:19:35 +0100

    Sort the object names, to make sure the lists are consistent.

--- tools/link-collisions/analyse.rb
+++ tools/link-collisions/analyse.rb
@@ -47,7 +47,7 @@ end
 db = PGconn.open(pg_params)
 
 db.exec("PREPARE getinstances (text, text) AS
-         SELECT name FROM symbols INNER JOIN objects ON symbols.object = objects.id WHERE symbol = $1 AND abi = $2")
+         SELECT name FROM symbols INNER JOIN objects ON symbols.object = objects.id WHERE symbol = $1 AND abi = $2 ORDER BY name")
 
 db.exec("SELECT * FROM duplicate_symbols").each do |row|
   outfile.puts "Symbol #{row[0]} (#{row[1]}) present #{row[2]} times"
