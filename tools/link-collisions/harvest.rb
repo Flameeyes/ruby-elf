eb2b9351a7bc88d2a5a8573c648420ecebd4b8d1
Diego Elio Pettenò <flameeyes@gmail.com>
2011-02-10 18:20:56 +0100

    link-collisions: rewrite harvester using the Elf::Tool base class.
    This reduces tremendously the duplicated code between Elf::Tool and the
    harvester, but also the harvester now uses PL/SQL on PostgreSQL to reduce
    the number of round-trips between Ruby and PostgreSQL when adding new
    implementations and symbols.
    
    Also this ensures that each object will add all its symbols with a single
    transaction, committed before closing the object, so that no
    halfway-through transactions are left when a file is not properly loaded.
    
    Note: starting from this version, the harvester is _always_ recursive in
    subdirectories.

--- tools/link-collisions/harvest.rb
+++ tools/link-collisions/harvest.rb
@@ -26,342 +26,263 @@ require 'pg'
 
 require 'elf'
 require 'elf/utils/loader'
+require 'elf/tools'
 
-opts = GetoptLong.new(
-  ["--no-scan-ldpath",     "-L", GetoptLong::NO_ARGUMENT ],
-  ["--scan-path",          "-p", GetoptLong::NO_ARGUMENT ],
-  ["--suppressions",       "-s", GetoptLong::REQUIRED_ARGUMENT ],
-  ["--multiplementations", "-m", GetoptLong::REQUIRED_ARGUMENT ],
-  ["--scan-directory",     "-d", GetoptLong::REQUIRED_ARGUMENT ],
-  ["--recursive-scan",     "-r", GetoptLong::NO_ARGUMENT ],
-  ["--elf-machine",        "-M", GetoptLong::REQUIRED_ARGUMENT ],
-  ["--postgres-username",  "-U", GetoptLong::REQUIRED_ARGUMENT ],
-  ["--postgres-password",  "-P", GetoptLong::REQUIRED_ARGUMENT ],
-  ["--postgres-hostname",  "-H", GetoptLong::REQUIRED_ARGUMENT ],
-  ["--postgres-port",      "-T",  GetoptLong::REQUIRED_ARGUMENT ],
-  ["--postgres-database",  "-D", GetoptLong::REQUIRED_ARGUMENT ]
-)
+module Elf::Tools
+  class CollisionsHarvester < Elf::Tool
+    def self.initialize
+      super
+      # this script doesn't support running multithreaded, since we
+      # need synchronous access to the database itself, so for now
+      # simply make sure that it never is executed with threads.
+      @execution_threads = nil
 
-suppression_files = File.exist?('suppressions') ? [ 'suppressions' ] : []
-multimplementation_files = File.exist?('multimplementations') ? [ 'multimplementations' ] : []
-scan_path = false
-scan_ldpath = true
-recursive_scan = false
-scan_directories = []
-$machines = []
+      @options |= [
+                   ["--no-scan-ldpath",     "-L", GetoptLong::NO_ARGUMENT ],
+                   ["--scan-path",          "-p", GetoptLong::NO_ARGUMENT ],
+                   ["--suppressions",       "-s", GetoptLong::REQUIRED_ARGUMENT ],
+                   ["--multiplementations", "-m", GetoptLong::REQUIRED_ARGUMENT ],
+                   ["--elf-machine",        "-M", GetoptLong::REQUIRED_ARGUMENT ],
+                   ["--postgres-username",  "-U", GetoptLong::REQUIRED_ARGUMENT ],
+                   ["--postgres-password",  "-P", GetoptLong::REQUIRED_ARGUMENT ],
+                   ["--postgres-hostname",  "-H", GetoptLong::REQUIRED_ARGUMENT ],
+                   ["--postgres-port",      "-T",  GetoptLong::REQUIRED_ARGUMENT ],
+                   ["--postgres-database",  "-D", GetoptLong::REQUIRED_ARGUMENT ]
+                  ]
 
-pg_params = {}
+      # we remove the -R option since we always want to be recursive in our search
+      @options.delete_if { |opt| opt[1] == "-R" }
+      @recursive = true
 
-opts.each do |opt, arg|
-  case opt
-  when '--suppressions'
-    unless File.exist? arg
-      $stderr.puts "harvest.rb: no such file or directory - #{arg}"
-      exit -1
+      @suppression_files = File.exist?('suppressions') ? [ 'suppressions' ] : []
+      @multimplementation_files = File.exist?('multimplementations') ? [ 'multimplementations' ] : []
+
+      # Total suppressions are for directories to skip entirely
+      # Partial suppressions are the ones that apply only to a subset
+      # of symbols.
+      @total_suppressions = []
+      @partial_suppressions = []
+
+      @multimplementations = []
     end
-    suppression_files << arg
-  when "--multiplementations"
-    unless File.exist? arg
-      $stderr.puts "harvest.rb: no such file or directory - #{arg}"
-      exit -1
+
+    def self.suppressions_cb(arg)
+      unless File.exist? arg
+        puterror("no such file or directory - #{arg}")
+        exit -1
+      end
+      @suppression_files << arg
     end
-    multimplementation_files << arg
-  when '--scan-path'
-    scan_path = true
-  when '--no-scan-ldpath'
-    scan_ldpath = false
-  when '--scan-directory'
-    scan_directories << arg
-  when '--recursive-scan'
-    recursive_scan = true
-  when "--elf-machine"
-    machine_str = arg.dup
 
-    # Remove the EM_ prefix if present
-    machine_str = machine_str[3..-1] if arg[0..2].upcase == "EM_"
-
-    # Remove underscores if present (we don't keep them in our
-    # constants)
-    machine_str.delete!("_")
-
-    machine_val = Elf::Machine.from_string(machine_str)
-
-    if machine_val.nil?
-      $stderr.puts "harvest.rb: unknown machine string - #{arg}"
-    else
-      $machines << machine_val unless machine_val.nil?
+    def self.multimplementations_cb(arg)
+      unless File.exist? arg
+        puterror("no such file or directory - #{arg}")
+        exit -1
+      end
+      @multimplementation_files << arg
     end
-  when '--postgres-username' then pg_params[:user] = arg
-  when '--postgres-password' then pg_params[:password] = arg
-  when '--postgres-hostname' then pg_params[:host] = arg
-  when '--postgres-port'     then pg_params[:port] = arg
-  when '--postgres-database' then pg_params[:dbname] = arg
-  end
-end
 
-$machines = nil if $machines.empty?
+    def self.elf_machine_cb(arg)
+      machine_str = (arg[0..2].upcase == "EM_" ? arg[3..-1] : arg).delete("_")
+      machine_val = Elf::Machine.from_string(machine_str)
 
-db = PGconn.open(pg_params)
-
-db.exec("DROP TABLE IF EXISTS symbols, multimplementations, objects CASCADE")
-
-db.exec("CREATE TABLE objects ( id INTEGER PRIMARY KEY, name VARCHAR(4096), abi VARCHAR(255), exported BOOLEAN, UNIQUE(name, abi) )")
-db.exec("CREATE TABLE multimplementations ( id INTEGER REFERENCES objects(id) ON DELETE CASCADE, path VARCHAR(4096), UNIQUE(path) )")
-db.exec("CREATE TABLE symbols ( object INTEGER REFERENCES objects(id) ON DELETE CASCADE, symbol TEXT,
-         PRIMARY KEY(object, symbol) )")
-
-db.exec("CREATE VIEW symbol_count AS
-         SELECT symbol, abi, COUNT(*) AS occurrences, BOOL_OR(objects.exported) AS exported FROM symbols INNER JOIN objects ON symbols.object = objects.id GROUP BY symbol, abi")
-db.exec("CREATE VIEW duplicate_symbols AS
-         SELECT * FROM symbol_count WHERE occurrences > 1 AND exported = 't' ORDER BY occurrences DESC, symbol ASC")
-
-db.exec("PREPARE newmulti (int, text) AS
-         INSERT INTO multimplementations (id, path) VALUES($1, $2)")
-db.exec("PREPARE newobject (int, text, text, boolean) AS
-         INSERT INTO objects(id, name, abi, exported) VALUES($1, $2, $3, $4)")
-db.exec("PREPARE newsymbol (int, text) AS
-         INSERT INTO symbols VALUES($1, $2)")
-
-db.exec("PREPARE checkimplementation(text, text) AS
-         SELECT id FROM objects WHERE name = $1 AND abi = $2")
-db.exec("PREPARE checkdupsymbol (int, text) AS
-         SELECT 1 FROM symbols WHERE object = $1 AND symbol = $2")
-
-# Total suppressions are for directories to skip entirely
-# Partial suppressions are the ones that apply only to a subset
-# of symbols.
-$total_suppressions = []
-$partial_suppressions = []
-
-suppression_files.each do |suppression|
-  File.open(suppression) do |file|
-    file.each_line do |line|
-      path, symbols = line.
-        gsub(/#\s.*/, '').
-        strip.
-        split(/\s+/, 2)
-      
-      next unless path
-      
-      if not symbols or symbols == ""
-        $total_suppressions << Regexp.new(path)
+      if machine_val.nil?
+        puterror("unknown machine string - #{arg}")
       else
-        $partial_suppressions << [Regexp.new(path), Regexp.new(symbols)]
-      end
-    end
-  end
-end
-
-multimplementations = []
-
-multimplementation_files.each do |multimplementation|
-  File.open(multimplementation) do |file|
-    file.each_line do |line|
-      implementation, paths = line.
-        gsub(/#\s.*/, '').
-        strip.
-        split(/\s+/, 2)
-
-      next unless implementation
-      next unless paths
-      
-      multimplementations << [ implementation, Regexp.new(paths) ]
-    end
-  end
-end
-
-so_files = Set.new
-
-# Extend Pathname with a so_files method
-class Pathname
-  def maybe_queue
-    return nil if symlink?
-
-    $total_suppressions.each do |supp|
-      return nil if to_s =~ supp
-    end
-
-    elf = nil
-
-    begin
-      elf = Elf::File.open(self)
-
-      if ($machines.nil? or $machines.include?(elf.machine)) and
-          (elf.has_section?('.dynsym') and elf.has_section?('.dynstr') and
-           elf.has_section?('.dynamic')) and
-          (elf[".dynsym"].class == Elf::SymbolTable)
-        return to_s
-      else
-        return nil
-      end
-    # Explicitly list this so that it won't pollute the output
-    rescue Elf::File::NotAnELF
-      return nil
-    rescue Exception => e
-      $stderr.puts "harvest.rb: #{e.message} - #{self.to_s}"
-    ensure
-      elf.close unless elf.nil?
-    end
-  end
-
-  def so_files(recursive = true)
-    res = Set.new
-    children.each do |entry|
-      begin
-        case entry.ftype
-        when "directory"
-          res.merge entry.so_files if recursive
-        when "file"
-          res << entry.maybe_queue
-        end
-        # When using C-c to stop, well, stop.
-      rescue Interrupt
-        raise
-      rescue Exception => e
-        $stderr.puts "Ignoring #{entry} (#{e.message})"
-        next
+        @machines ||= []
+        @machines << machine_val
       end
     end
 
-    return res
-  end
-end
+    def self.after_options
+      pg_params = {
+        :dbname   => @postgres_database,
+        :host     => @postgres_hostname,
+        :password => @postgres_password,
+        :port     => @postgres_port,
+        :user     => @postgres_username,
+      }
 
-if scan_ldpath
-  Elf::Utilities.system_library_path.each do |path|
-    begin
-      so_files.merge Pathname.new(path).so_files
-    rescue Errno::ENOENT
-      $stderr.puts "harvest.rb: No such file or directory - #{path}"
-      next
-    end
-  end
-end
+      @suppression_files.each do |suppression|
+        File.open(suppression) do |file|
+          file.each_line do |line|
+            path, symbols = line.
+              gsub(/#\s.*/, '').
+              strip.
+              split(/\s+/, 2)
 
-if scan_path and ENV['PATH']
-  ENV['PATH'].split(":").each do |path|
-    begin
-      so_files.merge Pathname.new(path).so_files(false)
-    rescue Errno::ENOENT
-      $stderr.puts "harvest.rb: No such file or directory - #{path}"
-      next
-    end
-  end
-end
+            next unless path
 
-scan_directories.each do |path|
-  begin
-    so_files.merge Pathname.new(path).so_files(recursive_scan)
-  rescue Errno::ENOENT
-    $stderr.puts "harvest.rb: No such file or directory - #{path}"
-    next
-  end
-end
-
-# if there are explicit files listed in the standard input, scan those
-# right away.
-ARGV.each do |path|
-  so_files << Pathname.new(path).maybe_queue
-end
-
-# finally if none of the above matched, try checking for data on the
-# standard input
-if ARGV.size == 0 and not scan_path and scan_directories.size == 0
-  $stdin.each_line do |path|
-    so_files << Pathname.new(path.chomp("\n")).maybe_queue
-  end
-end
-
-so_files.delete(nil)
-
-db.exec("BEGIN TRANSACTION")
-val = 0
-
-begin
-  require 'progressbar'
-
-  pbar = ProgressBar.new("harvest", so_files.size)
-rescue LoadError, NameError
-end
-
-so_files.each do |so|
-  local_suppressions = $partial_suppressions.dup.delete_if { |s| not so.to_s =~ s[0] }
-
-  begin
-    Elf::File.open(so) do |elf|
-      name = so
-      abi = "#{elf.elf_class} #{elf.abi} #{elf.machine.to_s.gsub("'", "\\'" )}"
-
-      impid = nil
-
-      multimplementations.each do |implementation, paths|
-        # Get the full matchdata because we might need to get the matches.
-        match = paths.match(so)
-
-        next unless match
-
-        while implementation =~ /\$([0-9]+)/ do
-          match_idx = $1.to_i
-          replacement = match[match_idx]
-          replacement = "" if replacement.nil?
-          implementation = implementation.gsub("$#{match_idx}", replacement)
-        end
-
-        name = implementation
-        db.exec("EXECUTE checkimplementation('#{implementation}', '#{abi}')").each do |row|
-          impid = row['id']
-        end
-        break
-      end
-
-      shared = (so != name) || (elf['.dynamic'].soname != nil)
-
-      unless impid
-        val += 1
-        impid = val
-        
-        db.exec("EXECUTE newobject(#{impid}, '#{name}', '#{abi}', '#{shared}')")
-      end
-
-      db.exec("EXECUTE newmulti(#{impid}, '#{so}')") if so != name
-        
-      elf['.dynsym'].each do |sym|
-        begin
-          next if sym.idx == 0 or
-            sym.bind != Elf::Symbol::Binding::Global or
-            sym.section.nil? or
-            sym.value == 0 or
-            sym.section.is_a? Integer or
-            sym.section.name == '.init' or
-            sym.section.name == '.bss'
-
-          skip = false
-          
-          local_suppressions.each do |supp|
-            if sym.name =~ supp[1]
-              skip = true
-              break
+            if not symbols or symbols == ""
+              @total_suppressions << Regexp.new(path)
+            else
+              @partial_suppressions << [Regexp.new(path), Regexp.new(symbols)]
             end
           end
-
-          next if skip or (db.exec("EXECUTE checkdupsymbol('#{impid}', '#{sym.name}@#{sym.version}')").num_tuples > 0)
-
-          db.exec("EXECUTE newsymbol('#{impid}', '#{sym.name}@#{sym.version}')")
-          
-        rescue Exception
-          $stderr.puts "Mangling symbol #{sym.name}"
-          raise
         end
       end
+
+      @total_suppressions = Regexp.union(@total_suppressions)
+
+      @multimplementation_files.each do |multimplementation|
+        File.open(multimplementation) do |file|
+          file.each_line do |line|
+            implementation, paths = line.
+              gsub(/#\s.*/, '').
+              strip.
+              split(/\s+/, 2)
+
+            next unless implementation
+            next unless paths
+
+            @multimplementations << [ implementation, Regexp.new(paths) ]
+          end
+        end
+      end
+
+      @targets |=
+        ( !@no_scan_ldpath             ? Elf::Utilities.system_library_path : [] ) |
+        ( (@scan_path and ENV['PATH']) ? ENV['PATH'].split(":")             : [] )
+
+      @db = PGconn.open(pg_params)
+
+      @db.exec("DROP TABLE IF EXISTS symbols, multimplementations, objects CASCADE")
+      @db.exec("DROP LANGUAGE IF EXISTS plpgsql CASCADE");
+
+      @db.exec("CREATE LANGUAGE plpgsql");
+      @db.exec("CREATE TABLE objects ( id SERIAL PRIMARY KEY, name VARCHAR(4096), abi VARCHAR(255), exported BOOLEAN, UNIQUE(name, abi) )")
+      @db.exec("CREATE TABLE multimplementations ( id INTEGER REFERENCES objects(id) ON DELETE CASCADE, path VARCHAR(4096), UNIQUE(path) )")
+      @db.exec("CREATE TABLE symbols ( object INTEGER REFERENCES objects(id) ON DELETE CASCADE, symbol TEXT,
+         PRIMARY KEY(object, symbol) )")
+
+      @db.exec("CREATE VIEW symbol_count AS
+         SELECT symbol, abi, COUNT(*) AS occurrences, BOOL_OR(objects.exported) AS exported FROM symbols INNER JOIN objects ON symbols.object = objects.id GROUP BY symbol, abi")
+      @db.exec("CREATE VIEW duplicate_symbols AS
+         SELECT * FROM symbol_count WHERE occurrences > 1 AND exported = 't' ORDER BY occurrences DESC, symbol ASC")
+
+      @db.exec("PREPARE newmulti (int, text) AS
+         INSERT INTO multimplementations (id, path) VALUES($1, $2)")
+
+      @db.exec(<<EOF)
+CREATE FUNCTION implementation (
+    p_implementation TEXT,
+    p_abi TEXT,
+    p_exported BOOLEAN,
+    OUT implementation_id INTEGER,
+    OUT created BOOLEAN
+) AS '
+  BEGIN
+    SELECT INTO implementation_id id FROM objects
+      WHERE name = p_implementation AND abi = p_abi;
+
+    IF implementation_id IS NULL THEN
+      created := TRUE;
+
+      INSERT INTO objects(name, abi, exported)
+        VALUES(p_implementation, p_abi, p_exported);
+      SELECT INTO implementation_id
+        currval(pg_get_serial_sequence(''objects'', ''id''));
+    END IF;
+  END;
+' LANGUAGE 'plpgsql';
+EOF
+
+      @db.exec(<<EOF)
+CREATE FUNCTION symbol (
+    p_object INTEGER,
+    p_symbol TEXT
+) RETURNS VOID AS '
+  BEGIN
+    INSERT INTO symbols VALUES(p_object, p_symbol);
+    RETURN;
+  EXCEPTION
+    WHEN unique_violation THEN
+      RETURN;
+  END;
+' LANGUAGE 'plpgsql';
+EOF
     end
-  rescue Exception
-    $stderr.puts "Checking #{so}"
-    raise
+
+    def self.db_exec(query)
+      @db.exec(query)
+    end
+
+    def self.analysis(filename)
+      return if filename =~ @total_suppressions
+
+      Elf::File.open(filename) do |elf|
+        unless ($machines.nil? or $machines.include?(elf.machine)) and
+            (elf.has_section?('.dynsym') and elf.has_section?('.dynstr') and
+             elf.has_section?('.dynamic')) and
+            (elf[".dynsym"].class == Elf::SymbolTable)
+          return
+        end
+
+        local_suppressions = Regexp.union((@partial_suppressions.dup.delete_if{ |s| filename.to_s !~ s[0] }).collect { |s| s[1] })
+
+        name = filename
+        abi = "#{elf.elf_class} #{elf.abi} #{elf.machine}".gsub("'", "''")
+
+        @multimplementations.each do |implementation, paths|
+          # Get the full matchdata because we might need to get the matches.
+          match = paths.match(filename)
+
+          next unless match
+
+          while implementation =~ /\$([0-9]+)/ do
+            match_idx = $1.to_i
+            replacement = match[match_idx]
+            replacement = "" if replacement.nil?
+            implementation = implementation.gsub("$#{match_idx}", replacement)
+          end
+
+          name = implementation
+          break
+        end
+
+        shared = (filename != name) || (elf['.dynamic'].soname != nil)
+
+        res = db_exec("SELECT * FROM implementation('#{name}', '#{abi}', '#{shared}')")
+        impid = res[0]["implementation_id"]
+
+        if filename != name
+          # If this is a collapsed multimplementation, add it to the list
+          db_exec("EXECUTE newmulti(#{impid}, '#{filename}')")
+        elsif res[0]["created"] != "t"
+          # otherwise we simply skip over it because we've already seen the file!
+          next
+        end
+
+        query = ""
+        elf['.dynsym'].each do |sym|
+          begin
+            next if sym.idx == 0 or
+              sym.bind != Elf::Symbol::Binding::Global or
+              sym.section.nil? or
+              sym.value == 0 or
+              sym.section.is_a? Integer or
+              sym.section.name == '.init' or
+              sym.section.name == '.bss'
+
+            next if (sym.name =~ local_suppressions);
+
+            query << "SELECT symbol('#{impid}', '#{sym.name}@#{sym.version}');"
+          rescue Exception
+            $stderr.puts "Mangling symbol #{sym.name}"
+            raise
+          end
+        end
+        db_exec("BEGIN TRANSACTION;" + query + "COMMIT;") unless query.empty?
+      end
+    end
+
+    def self.results
+      db_exec(<<EOF)
+BEGIN TRANSACTION;
+CREATE INDEX objects_name ON objects(name);
+CREATE INDEX symbols_symbol ON symbols(symbol);
+COMMIT;
+EOF
+    end
   end
-
-  pbar.inc if pbar
 end
-
-db.exec("CREATE INDEX objects_name ON objects(name)")
-db.exec("CREATE INDEX symbols_symbol ON symbols(symbol)")
-db.exec("COMMIT")
