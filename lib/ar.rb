4af20cfae66597c9e19e18fb0522d26f61a323e4
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2012-09-13 15:22:04 -0700

    Initial implementation of an ar format parser for Ruby-Elf.
    By implementing parsing for ar, it's possible to access the ELF
    objects within Unix static archives and analyze them without having to
    extract them beforehand.
    
    Right now this cannot be used for accessing files into memory; this
    will come later.

---
+++ lib/ar.rb
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+# Simple ar format parser for Ruby
+#
+# Copyright © 2012 Diego Elio Pettenò <flameeyes@flameeyes.eu>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this generator; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+require 'bytestream-reader'
+require 'pathname'
+require 'elf/utils/offsettable'
+
+module Ar
+  class File < ::File
+    include BytestreamReader
+
+    MagicString = "!<arch>\n"
+
+    class NotAnAR < Exception
+      def initialize
+        super("not a valid ar file")
+      end
+    end
+
+    def _checkvalidpath(path)
+      # We're going to check the path we're given for a few reasons,
+      # the first of which is that we do not want to open a pipe or
+      # something like that. If we were just to leave it to File.open,
+      # we would end up stuck waiting for data on a named pipe, for
+      # instance.
+      #
+      # We cannot just use File.file? either because it'll be ignoring
+      # ENOENT by default (which would be bad for us).
+      #
+      # If we were just to use File.ftype we would have to handle
+      # manually the links... since Pathname will properly report
+      # ENOENT for broken links, we're going to keep it this way.
+      path = Pathname.new(path) unless path.is_a? Pathname
+
+      case path.ftype
+      when "directory" then raise Errno::EISDIR
+      when "file" then # do nothing
+      when "link"
+        # we use path.realpath here; the reason is that if
+        # we're to use readlink we're going to have a lot of
+        # trouble to find the correct path. We cannot just
+        # always use realpath as that will run too many stat
+        # calls and have a huge hit on performance.
+        _checkvalidpath(path.realpath)
+      else
+        raise Errno::EINVAL
+      end
+    end
+
+    private :_checkvalidpath
+
+    def initialize(path)
+      _checkvalidpath(path)
+
+      super(path, "rb")
+
+      begin
+        begin
+          raise NotAnAR unless readexactly(MagicString.size) == MagicString
+        rescue EOFError
+          raise NotAnAR
+        end
+
+        @files = []
+        @files_by_name = {}
+
+        begin
+          until eof
+            # read the file header; most of the content is actually in
+            # an ASCII-compatible text format, which is very easy to
+            # parse
+            file = {
+              :name => readexactly(16).rstrip,
+              :mtime => Time.at(readexactly(12).rstrip.to_i),
+              :owner => readexactly(6).rstrip.to_i,
+              :group => readexactly(6).rstrip.to_i,
+              :mode => readexactly(8).rstrip.to_i(8),
+              :size => readexactly(10).rstrip.to_i
+            }
+
+            raise NotAnAR if readexactly(2) != "\x60\x0a"
+
+            case file[:name]
+            when "/"
+              # this is the index that has to be parsed
+              seek(file[:size] + (file[:size]%1), IO::SEEK_CUR)
+              next
+            when "//"
+              # this is the extended filename table, just read it and
+              # skip adding the file to the list.
+              @gnu_names = Elf::Utilities::OffsetTable.new(readexactly(file[:size]), "\n")
+            else
+              if file[:name][0..3] == "#1/"
+                file[:name] = readexactly(file[:name][4..-1].to_i)
+              end
+
+              if file[:name][0] == "/"
+                file[:name] = @gnu_names[file[:name][1..-1].to_i]
+              end
+              
+              # GNU ar terminates all filenames with a slash; remove it
+              # to have the actual filename. Keep / and // the same so
+              # that they can keep their defined names.
+              file[:name].sub!(/([^\/])\/$/, '\1')
+
+              file[:offset] = tell()
+
+              @files << file
+              @files_by_name[file[:name]] = file
+
+              seek(file[:size] + (file[:size]%1), IO::SEEK_CUR)
+            end
+          end
+        rescue EOFError
+          raise Errno::ENODATA
+        end
+      rescue ::Exception => e
+        close
+        raise e
+      end
+    end
+
+    def [](index_or_name)
+      if index_or_name.is_a?(Integer)
+        return @files[index_or_name]
+      else
+        return @files_by_name[index_or_name]
+      end
+    end
+
+    def files_count
+      @files.size
+    end
+  end
+end
---
+++ test/test_ar.rb
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*-
+# Tests for the ar format reader
+#
+# Copyright © 2012 Diego Elio Pettenò <flameeyes@flameeyes.eu>
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# 
+
+require 'test/unit'
+require 'tempfile'
+
+require 'ar'
+require 'tt_elf'
+
+class TC_AR < Test::Unit::TestCase
+  # We cannot use the fileno count trick on JRuby :(
+  if RUBY_PLATFORM != "java"
+    # Define setup and teardown functions to make sure that no
+    # descriptors are leaked during the tests. We don't want descriptors
+    # to leak when exception happens, otherwise we likely have a bug in
+    # the code.
+    def setup
+      file = File.new(get_test_file("invalid/nonelf"))
+      @fileno_before = file.fileno
+      file.close
+    end
+
+    def teardown
+      file = File.new(get_test_file("invalid/nonelf"))
+      @fileno_after = file.fileno
+      file.close
+
+      assert_equal(@fileno_before, @fileno_after,
+                   "Descriptor leaked!")
+    end
+  else
+    $stderr.puts "Unable to test for file descriptor leaks on JRuby"
+  end
+
+  # Helper to check for exceptions on opening a file.
+  def helper_open_exception(exception_class, subpath)
+    assert_raise exception_class do
+      Ar::File.new(get_test_file("invalid/#{subpath}"))
+    end
+  end
+
+  # Test behaviour when a file is requested that is not present.
+  #
+  # Expected behaviour: Errno::ENOENT exception is raised
+  def test_nofile
+    filepath = get_test_file("invalid/notfound")
+
+    # Check that the file does not exist or we're going to throw an
+    # exception to signal an error in the test.
+    if File.exists?(filepath)
+      raise Exception.new("File '#{filepath}' present in the test directory.")
+    end
+
+    helper_open_exception Errno::ENOENT, "notfound"
+  end
+
+  # Test behaviour when a file that is not an AR file is opened.
+  #
+  # Expected behaviour: Ar::File::NotAnAR exception is raised.
+  def test_notanar
+    helper_open_exception Ar::File::NotAnAR, "nonelf"
+  end
+
+  # Test behaviour when a file too short to be an AR file is opened
+  # (that has not enough data to read the eight magic bytes at the
+  # start of the file).
+  #
+  # Expected behaviour: Ar::File::NotAnAR exception is raised.
+  def test_shortfile
+    helper_open_exception Ar::File::NotAnAR, "shortfile"
+  end
+
+  # Test behaviour when opening a named pipe (fifo) path
+  #
+  # Expected behaviour: Errno::EINVAL exception is raised
+  def test_named_pipe
+    # Since we cannot add the pipe to the git repository, we've got to
+    # create one ourselves :(
+    pipedir = File.expand_path("ruby-elf-tests-#{Process.pid}-#{Time.new.strftime("%s")}", Dir.tmpdir)
+    pipepath = File.expand_path("fifo", pipedir)
+    begin
+      Dir.mkdir(pipedir)
+      system("mkfifo #{pipepath}")
+
+      assert_raise Errno::EINVAL do
+        Ar::File.new(pipepath)
+      end
+    ensure
+      FileUtils.rmtree(pipedir)
+    end
+  end
+
+  # Test behaviour when opening a file too short to contain a file
+  # header.
+  def test_truncated
+    helper_open_exception Errno::ENODATA, "truncatedarchive"
+  end
+
+  # Test behaviour when opening a file that has an invalid
+  # end-of-header signature.
+  def test_invalidendfile
+    helper_open_exception Ar::File::NotAnAR, "invalidendfile"
+  end
+
+  # Test access of a simple simplifed file
+  def test_singlefile
+    ar = Ar::File.new(get_test_file("archive/singlefile.a"))
+    assert_equal 1, ar.files_count
+    file = ar[0]
+
+    # check the file's header
+    assert_equal "shortfile", file[:name]
+    assert_equal 501, file[:owner]
+    assert_equal 1005, file[:group]
+    assert_equal 0100644, file[:mode] #octal
+    assert_equal 2, file[:size]
+
+    # make sure that it was inserted into the index correctly
+    assert_equal file, ar["shortfile"]
+  ensure
+    ar.close unless ar.nil?
+  end
+
+  # Test access of a file with GNU long file names
+  def test_longfilename_gnu
+    ar = Ar::File.new(get_test_file("archive/longfilename-gnu.a"))
+    assert_equal 1, ar.files_count
+    file = ar[0]
+
+    assert_equal file[:name], "thisisaveryveryverylongfilenameandithastobeencodedwiththeextendedformattoworkcorrectly"
+    
+    assert_equal file, ar["thisisaveryveryverylongfilenameandithastobeencodedwiththeextendedformattoworkcorrectly"]
+  ensure
+    ar.close unless ar.nil?
+  end
+
+  # Test access of a file with real objects (GNU ar)
+  def test_objects_gnu
+    ar = Ar::File.new(get_test_file("archive/objects-gnu.a"))
+    assert_equal 3, ar.files_count
+  ensure
+    ar.close unless ar.nil?
+  end
+end
